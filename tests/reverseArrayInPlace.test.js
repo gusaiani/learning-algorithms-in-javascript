477221fee73b2ca986e9a0dc4deade0db2b08922
Gustavo Saiani <gustavo@poe.ma>
2018-06-05 14:53:54 +0200

    Add solution for reverse array in place with for

--- tests/reverseArrayInPlace.test.js
+++ tests/reverseArrayInPlace.test.js
@@ -3,3 +3,7 @@ const reverseArrayInPlace = require('../reverseArrayInPlace');
 test('Reverse [0, 1, 2]', () => {
   expect(reverseArrayInPlace([0, 1, 2])).toEqual([2, 1, 0]);
 });
+
+test('Reverse [0, 1, 2, 3, 4, 5]', () => {
+  expect(reverseArrayInPlace([0, 1, 2, 3, 4, 5])).toEqual([5, 4, 3, 2, 1, 0]);
+});
