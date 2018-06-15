9f7db4fca6b5410c45526fc83205b3f9ef973316
Gustavo Saiani <gustavo@poe.ma>
2018-06-15 11:55:11 +0200

    Solve twoSum with O(n^2) time complexity

--- twoSum.js
+++ twoSum.js
@@ -1,6 +1,16 @@
-function twoSum(arr, sum) {
-  return [1];
+function twoSumON2TimeComplexity(arr, sum) {
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] + arr[i] == sum) {
+        result.push([arr[j], arr[i]]);
+      }
+    }
+  }
+
+  return result;
 }
 
-module.exports = twoSum;
+module.exports = twoSumON2TimeComplexity;
 
