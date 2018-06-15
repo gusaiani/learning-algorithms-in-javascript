921d377584ba76ad53330d9cc4719e30af10f358
Gustavo Saiani <gustavo@poe.ma>
2018-06-15 14:24:16 +0200

    TwoSum implemented in ON Time Complexity

--- twoSum.js
+++ twoSum.js
@@ -12,5 +12,57 @@ function twoSumON2TimeComplexity(arr, sum) {
   return result;
 }
 
-module.exports = twoSumON2TimeComplexity;
+function firstAttemptAtTwoSumONTimeComplexity(arr, sum) {
+  /*
+   * This attempt almost works, and at the same time is much less elegant
+   * and efficient than the following.
+   * Since the hashTable as object doesn't preserve the order with which
+   * the array is presented, I was able to return an array with almost identical items,
+   * the difference being that the inner arrays sometimes come in reversed orders.
+  */
+  let hashTable = {};
+  let result = [];
+
+  arr.forEach(item => {
+    if (!hashTable[item]) hashTable[item] = 0;
+    hashTable[item]++;
+  })
+
+  const hashKeys = Object.keys(hashTable);
+
+  for (let i = 0; i < hashKeys.length / 2; i ++) {
+    const keyInt = parseInt(hashKeys[i]);
+    const keyToSearch = sum - keyInt;
+    const hasValidSum = hashKeys.indexOf(keyToSearch.toString()) > i;
+
+    if (hasValidSum) {
+      const numberOfRepetitions = hashTable[hashKeys[i]] * hashTable[keyToSearch];
+
+      for (let j = 0; j < numberOfRepetitions; j++) {
+        result.push([keyToSearch, keyInt]);
+      }
+    }
+  }
+
+  return result;
+}
+
+function twoSumONTimeComplexity(numArray, sum) {
+  var pairs = [];
+  var hashtable = [];
+
+  for (let i = 0; i < numArray.length; i++) {
+    var currNum = numArray[i];
+    var counterpart = sum - numArray[i];
+
+    if (hashtable.indexOf(counterpart) !== -1) {
+      pairs.push([ currNum, counterpart ]);
+    }
+    hashtable.push(currNum);
+  }
+
+  return pairs;
+}
+
+module.exports = twoSumONTimeComplexity;
 
