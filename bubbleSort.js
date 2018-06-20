a3ca0454293421c4f9d77601254499225344ffe3
Gustavo Saiani <gustavo@poe.ma>
2018-06-20 12:39:55 +0200

    A solution for bubble sort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,5 +1,17 @@
 function bubbleSort(arr) {
-  return [0];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i; j++) {
+      const item1 = arr[j];
+      const item2 = arr[j + 1];
+
+      if (item1 > item2) {
+        arr[j] = item2;
+        arr[j + 1] = item1;
+      }
+    }
+  }
+
+  return arr;
 }
 
 module.exports = bubbleSort;
--- tests/bubbleSort.test.js
+++ tests/bubbleSort.test.js
@@ -8,3 +8,10 @@ test('Array with 6 items', () => {
   );
 });
 
+test('Array with 6 items', () => {
+  const unsortedArray = [5, 3, 8, 2, 1, 4];
+
+  expect(bubbleSort(unsortedArray)).toEqual(
+    [1, 2, 3, 4, 5, 8]
+  );
+});
