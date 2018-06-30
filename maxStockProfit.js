2c62a00bc5f778018f9b9cddda344ffe8898b08e
Gustavo Saiani <gustavo@poe.ma>
2018-06-30 20:07:01 -0300

    Consider no-profit situations in maxStockProfit

--- maxStockProfit.js
+++ maxStockProfit.js
@@ -1,11 +1,16 @@
 function maxStockProfit(arr) {
-  const {min, max} = arr.reduce((acc, price) => {
+  let firstMin = firstMax = arr.shift();
+
+  const {min, max, anyUpwardMotion, anyDownwardMotion} = arr.reduce((acc, price) => {
+    if (price > acc.min) { acc.anyUpwardMotion = true }
+    if (price < acc.max) { acc.anyDownwardMotion = true }
     if (price < acc.min) { acc.min = price };
     if (price > acc.max) { acc.max = price };
     return acc;
   },
-  {min: Infinity, max: -Infinity});
+    {min: firstMin, max: firstMax, anyUpwardMotion: false, anyDownwardMotion: false});
 
+  if (!anyUpwardMotion && anyDownwardMotion) return -1;
   return max - min;
 }
 
--- tests/maxStockProfit.test.js
+++ tests/maxStockProfit.test.js
@@ -4,3 +4,13 @@ test('Array with 6 items', () => {
   const arr = [32, 46, 26, 38, 40, 48, 42];
   expect(maxStockProfit(arr)).toEqual(22);
 });
+
+test('A profit of zero should return zero', () => {
+  const arr = [5, 5, 5, 5, 5];
+  expect(maxStockProfit(arr)).toEqual(0);
+});
+
+test('No profit should return -1', () => {
+  const arr = [6, 5, 4, 3, 2, 1];
+  expect(maxStockProfit(arr)).toEqual(-1);
+});
