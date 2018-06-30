bc57ada9d1a86afee22cedda8980b5886dce73fa
Gustavo Saiani <gustavo@poe.ma>
2018-06-30 18:56:03 -0300

    Add another test for mergeSort

--- tests/mergeSort.test.js
+++ tests/mergeSort.test.js
@@ -8,3 +8,10 @@ test('Array with 6 items', () => {
   );
 });
 
+test('Array with 11 items', () => {
+  const unsortedArray = [6000, 34, 203, 3, 746, 984, 198, 764, 1, 9, 1];
+
+  expect(mergeSort(unsortedArray)).toEqual(
+    [1, 1, 3, 9, 34, 198, 203, 746, 764, 984, 6000]
+  );
+});
